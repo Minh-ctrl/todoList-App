a9a1c68b3f01e90d21a18ef096d4085c4d28ef49
minh-ctrl <binhminh140899@gmail.com>
2022-04-25 15:52:02 +0900

    feat: 🎸 svg overflow

--- client/src/components/BobbleComponent.vue
+++ client/src/components/BobbleComponent.vue
@@ -6,9 +6,11 @@
 
 <template>
     <div>
-        <div v-for="svg in svgs" :key="svg.name">
-            <Icon :svg="svg"/>
-        </div>    
+        <div class="flex flex-row flex-nowrap justify-center overflow-x-auto">
+            <div class=" w-20 h-20 ml-2 mr-2 " v-for="svg in svgs" :key="svg.name">
+                <Icon :svg="svg"/>
+            </div>    
+        </div>
     </div>
 </template>
 
