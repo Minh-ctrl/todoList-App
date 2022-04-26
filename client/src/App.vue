05030e8d4dd4740999d366e326c5590d4794f2f9
minh-ctrl <binhminh140899@gmail.com>
2022-04-26 14:10:16 +0900

    feat: 🎸 implementing Today component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,29 @@
 <script setup>
+  import {ref, reactive} from 'vue';
   import BobbleComponent from './components/BobbleComponent.vue';
-  let data = [{name:'Chat'}, {name: 'Camera'}, {name: 'Cleaning'}, {name: 'Mail'} , {name: "Study"}, {name:"Shopping"}, {name:"Pill"}];
+  import Today from './components/Today.vue'
+
+  let svg = [{name:'Chat'}, {name: 'Camera'}, {name: 'Cleaning'}, {name: 'Mail'} , {name: "Study"}, {name:"Shopping"}, {name:"Pill"}];
+  const  data= ref({today:{
+    todo: [{
+      action:'Talk to Mom',
+      timer: 50
+
+      },{
+        action:'finish assignment',
+        timer: 50,
+      },
+      {
+        action:'finish this project'
+      }
+      ]
+  }});
 </script>
 
 <template>
   <div>
-    <BobbleComponent :svg='data'/>
+    <BobbleComponent :svg='svg'/>
+    <Today :data="data"/>
   </div>
 </template>
 <style>
---
+++ client/src/components/Today.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import {ref, computed} from 'vue';
+    const props = defineProps(['data']);
+    const todos = props.data.today.todo;
+</script>
+<template>
+    <div class="flex flex-col flex-nowrap">
+        <h1 class="text-4xl font-semibold text-cyan-400">
+            TODAY
+        </h1>
+        <div>
+            <div v-for="todo in todos" :key="todo">
+                <input type="checkbox" class="m-2">
+                <label>{{todo.action}}</label>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
